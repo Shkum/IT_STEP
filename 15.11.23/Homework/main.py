6c8812f84f3c64724e4ac7fa2ebf28e748089650
Sergiy <shkum79@ukr.net>
2023-11-17 01:19:08 +0100

    Homework 3

--- 15.11.23/Homework/main.py
+++ 15.11.23/Homework/main.py
@@ -8,16 +8,25 @@
 ■ Square — квадрат із заданими з координатами лівого
 верхнього кута та довжиною сторони.
 ■ Rectangle — прямокутник із заданими координатами
-верхнього лівого кута та розмірами.'''
+верхнього лівого кута та розмірами.
+■ Circle — коло із заданими координатами центру та радіусом.
+■ Ellipse — еліпс із заданими координатами верхнього кута
+описаного навколо нього прямокутника зі сторонами,
+паралельними осям координат, та розмірами цього прямокутника.
+Створіть список фігур, збережіть фігури у файл, завантажте в інший список та відобразіть інформацію про кожну
+фігуру'''
 from dataclasses import dataclass
 import turtle
 
 
 @dataclass
 class Shape:
-    sideA: int
+    sideA: int = 0
     sideB: int = 0
     left_upper_corner_pos: list = (0, 0)
+    radius: int = 0
+    center_pos: list = (0, 0)
+    rectangle_around_ellipse: list = (0, 0, 100, 200)
 
     def show(self):
         cur = turtle.Turtle()
@@ -37,7 +46,12 @@ class Shape:
 
     def save(self, file_name):
         with open(file_name, 'w', encoding='UTF-8') as f:
-            f.writelines(f'{self.sideA}\n{self.sideB}\n{self.left_upper_corner_pos}')
+            f.writelines(f'{self.sideA}\n'
+                         f'{self.sideB}\n'
+                         f'{self.left_upper_corner_pos}\n'
+                         f'{self.radius}\n'
+                         f'{self.center_pos}\n'
+                         f'{self.rectangle_around_ellipse}')
 
     def load(self, file_name):
         with open(file_name, encoding='UTF-8') as f:
@@ -45,6 +59,9 @@ class Shape:
             self.sideA = int(lines[0])
             self.sideB = int(lines[1])
             self.left_upper_corner_pos = eval(lines[2])
+            self.radius = eval(lines[3])
+            self.center_pos = eval(lines[4])
+            self.rectangle_around_ellipse = eval(lines[4])
 
 
 @dataclass
@@ -57,17 +74,51 @@ class Rectangle(Shape):
     pass
 
 
+@dataclass
+class Circle(Shape):
+
+    def show(self):
+        turtle.circle(self.radius)
+        turtle.exitonclick()
+        turtle.TurtleScreen._RUNNING = True
+
+
+@dataclass
+class Ellipse(Shape):
+
+    def show(self):
+        turtle.seth(-45)
+        for i in range(2):
+            turtle.circle(self.radius, 90)
+            turtle.circle(self.radius // 2, 90)
+        turtle.exitonclick()
+        turtle.TurtleScreen._RUNNING = True
+
+
 square = Square(200)
 rectangle = Rectangle(150, 300, [300, 300])
 
 square.show()
 rectangle.show()
 
+circle = Circle(radius=150)
+circle.show()
+
+ellipse = Ellipse(radius=150)
+ellipse.show()
+
 square.save('square.txt')
 rectangle.save('rectangle.txt')
+circle.save('circle.txt')
+ellipse.save('ellipse.txt')
 
 square.load('square.txt')
 rectangle.load('rectangle.txt')
+circle.load('circle.txt')
+ellipse.load('ellipse.txt')
 
 square.show()
 rectangle.show()
+circle.show()
+ellipse.show()
+
