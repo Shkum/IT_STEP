209ea90df92139422c3f91fe97701ed908e135b4
Sergiy <shkum79@ukr.net>
2023-11-28 01:05:01 +0100

    Practical 2

--- 28.11.23/Lab/main.py
+++ 28.11.23/Lab/main.py
@@ -12,3 +12,119 @@
 після чого меню з’являється знову.
 '''
 
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return f'[{self.data}] -> {self.next}'
+
+
+node = Node(1)
+current_node = node
+
+for i in range(2, 10):
+    new_node = Node(i ** 2)
+    current_node.next = new_node
+    current_node = new_node
+
+print(node)
+
+print()
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        return f'{self.head}'
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def replace(self, old_data, new_data):
+        current = self.head
+        while current:
+            if current.data == old_data:
+                current.data = new_data
+                print(f'Value "{old_data}" replaced with "{new_data}"')
+                return
+            current = current.next
+        print(f'Value "{old_data}" not found')
+
+    def delete(self, data):
+        current = self.head
+        prev = current
+        while current:
+            if current.data == data:
+                if prev == current:
+                    self.head = current.next
+                    print(f'Value "{data}" deleted')
+                    return
+                if current.next is None:
+                    prev.next = None
+                    print(f'Value "{data}" deleted')
+                    return
+                prev.next = current.next
+                print(f'Value "{data}" deleted')
+                return
+            prev = current
+            current = current.next
+        print(f'Value "{data}" not found')
+
+    def find_value(self, value):
+        current = self.head
+        while current:
+            if current.data == value:
+                print(f'Value "{value}" found in LinkedList')
+                return
+            current = current.next
+        print(f'Value "{value}" not found')
+
+
+my_list = LinkedList()
+my_list.append("1")
+my_list.append('2')
+my_list.append('3')
+my_list.append('4')
+my_list.append('5')
+print(my_list)
+
+lst = []
+st = ' '
+while st:
+    st = input("Enter strings divided by ENTER (empty string to stop): ")
+    lst.append(st)
+
+for i in lst:
+    my_list.append(i)
+
+print(my_list)
+
+old_value = input("Enter string to change: ")
+new_value = input("Enter new string: ")
+
+my_list.replace(old_value, new_value)
+print(my_list)
+
+my_list.delete('1')
+print(my_list)
+
+my_list.delete('3')
+print(my_list)
+
+my_list.delete('20')
+print(my_list)
+
+my_list.find_value('3')
+my_list.find_value('4')
