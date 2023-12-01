8a63e35170300e4a1e4bf9c7d9e5c191ff9a277a
Sergiy <shkum79@ukr.net>
2023-12-01 02:04:28 +0100

    Homework 03 History added

--- 29.11.23/Homework/main.py
+++ 29.11.23/Homework/main.py
@@ -19,9 +19,9 @@ from collections import deque
 # Напомните :) показать интересную фишку показывающую разницу между
 # статическим и динамическим объявлением атрибута класса :)
 class Stack:
-    # _stack = deque()
     counter = 0
 
+    # _stack = deque()
     def __init__(self):
         self._stack = deque()
 
@@ -35,16 +35,26 @@ class Stack:
         Stack.counter += 1
         return self._stack.pop()
 
+    def val(self):
+        return self._stack
 
-def rebuild_towers(n: int, a: Stack, b: Stack, c: Stack):
+    def __iter__(self):
+        return iter(self._stack)
+
+
+
+def rebuild_towers(n: int, a: Stack, b: Stack, c: Stack, history: Stack):
     if n > 0:
-        rebuild_towers(n - 1, a, c, b)
+        rebuild_towers(n - 1, a, c, b, history)
         if a:
+            history.add_value([list(a), list(b), list(c)])
             c.add_value(a.pop_value())
-        rebuild_towers(n - 1, b, a, c)
+        rebuild_towers(n - 1, b, a, c, history)
+    return history
 
 
 n = 15
+history = Stack()
 a = Stack()
 for i in range(n):
     a.add_value(n - i)
@@ -54,15 +64,16 @@ print('Stack a:', a)
 print('Stack b:', b)
 print('Stack c:', c)
 
-rebuild_towers(n, a, b, c)
+history = rebuild_towers(n, a, b, c, history)
 
 print('\nRebuilding of towers ...')
 print('\nRebuilding of towers completed\n')
-print(f'It takes {Stack.counter} operations\n')
+print(f'It used {Stack.counter} operations\n')
 
 print('Stack a:', a)
 print('Stack b:', b)
 print('Stack c:', c)
+# print(history)
 
 
 # print(f"\n{'?' * 100}\n")
